114a5d3adc82437013c764629736b15cd14da0ad
bilal-faisal <bilalfaisal.developer@gmail.com>
2024-01-04 20:43:23 +0500

    restructure api-clients route to token route

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -2,12 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import postgres from "postgres";
 
 export async function POST(request: NextRequest) {
-  const req = await request.json();
+  let req: any = {}
+  try {
+    req = await request.json();
+  } catch (e) {
+    return NextResponse.json({ "error": "No body found with request" });
+  }
+
   if (req.clientName && req.clientEmail) {
     const conn = postgres({
       ssl: require,
     });
-    const result = await conn.unsafe(`SELECT * FROM clients where clientname = '${req.clientName}' and clientemail = '${req.clientEmail}'`);
+
+    const result = await conn.unsafe(`SELECT * FROM clients where clientemail = '${req.clientEmail}'`);
+
     if (result.length === 0) {
       // generate new token, check, save it, and then send response
       let token = Math.floor(Math.random() * 9000000000) + 1000000000;
@@ -17,20 +25,20 @@ export async function POST(request: NextRequest) {
         exists = await conn.unsafe(`SELECT * FROM clients where token = ${token}`);
       }
 
-      // also add client id
       // select all distinct bearer tokens
       let countResult = await conn.unsafe(`SELECT COUNT(DISTINCT token) FROM clients;`);
       let count = +countResult[0]["count"];
 
       const entry = await conn.unsafe(`INSERT INTO clients (token, clientName, clientEmail, clientId) VALUES (${token}, '${req.clientName}', '${req.clientEmail}', '${++count}');`);
       // console.log(entry)
-      return new NextResponse(JSON.stringify({ token: token }));
+      
+      return NextResponse.json({ token: token });
 
     } else {
-      return new NextResponse(JSON.stringify({ token: result[0]['token'] }));
+      return NextResponse.json({ token: result[0]['token'] });
     }
   }
   else {
-    return new NextResponse('Please include clientName & clientEmail in the POST request');
+    return NextResponse.json({ "error": "Please include clientName & clientEmail in the POST request" });
   }
 }
