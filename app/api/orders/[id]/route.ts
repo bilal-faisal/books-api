311ef090dfdda128a697fb2e543384b31d23e1be
bilal-faisal <bilalfaisal.developer@gmail.com>
2024-01-07 23:53:10 +0500

    updated orders route

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,32 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import postgres from "postgres";
 
-// async function verifyTokenCall(token: string) {
-//     const response = await fetch(
-//         `http://localhost:3000/api/verifyToken`,
-//         {
-//             body: JSON.stringify({ "token": token }),
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             method: 'POST'
-//         }
-//     );
-//     return response.json();
-// }
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    let bearerToken = (request.headers.get('authorization'))
-    // if (!bearerToken) {
-    //     return new NextResponse('This endpoint requires bearer token');
-    // } else {
-    const token = bearerToken?.split(' ')[1];
-    // let data = await verifyTokenCall(token);
-    // if (data.status == 'invalid') {
-    //     return new NextResponse("Invalid Token");
-    // } else {
+
     let id = parseInt(params.id);
+
     if (!isNaN(id)) {
+        // Authorization already verified by middleware but we still need the token to get data
+        const bearerToken = (request.headers.get('authorization'))
+        const token = bearerToken?.split(' ')[1];
+
         const conn = postgres({
             ssl: require,
         });
@@ -35,78 +19,71 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
 
         const result = await conn.unsafe(`SELECT * FROM orders WHERE orderid = ${id} AND clientId = ${clientId}`);
         if (result.length > 0) {
-            return new NextResponse(JSON.stringify(result));
+            return NextResponse.json(result[0]);
         } else {
-            return new NextResponse(JSON.stringify({
-                error: `No order with id ${id}`
-            }));
+            return NextResponse.json({
+                error: `No order exists with id no. ${id}`
+            });
         }
     } else {
-        return new NextResponse(JSON.stringify({
-            error: `No book with id ${id}`
-        }));
+        return NextResponse.json({
+            error: `Invalid order id: ${id}`
+        });
     }
-    // }
-    // }
 }
 
-
 export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-    let bearerToken = (request.headers.get('authorization'))
-    // if (!bearerToken) {
-    //     return new NextResponse('This endpoint requires bearer token');
-    // } else {
-    const token = bearerToken?.split(' ')[1];
-    // let data = await verifyTokenCall(token);
-    // if (data.status == 'invalid') {
-    //     return new NextResponse("Invalid Token");
-    // } else {
+
     let id = parseInt(params.id);
 
     if (!isNaN(id)) {
+        // Authorization already verified by middleware but we still need the token to get data
+        const bearerToken = (request.headers.get('authorization'))
+        const token = bearerToken?.split(' ')[1];
+
         const conn = postgres({
             ssl: require,
         });
         const tokenResult = await conn.unsafe(`SELECT clientId FROM clients where token = '${token}'`);
         let clientId = +tokenResult[0]["clientid"];
-
 
         const result = await conn.unsafe(`SELECT * FROM orders WHERE orderid = ${id} AND clientId = ${clientId}`);
         if (result.length > 0) {
             const result = await conn.unsafe(`DELETE FROM orders WHERE orderid = ${id} AND clientId = ${clientId}`);
-            return new NextResponse("Deleted");
+
+            return NextResponse.json({
+                message: `Order with id no ${id} deleted successfully`
+            });
         } else {
-            return new NextResponse(JSON.stringify({
-                error: `No order with id ${id}`
-            }));
+            return NextResponse.json({
+                error: `No order exists with id no. ${id}`
+            });
         }
     } else {
-        return new NextResponse(JSON.stringify({
-            error: `Invalid id ${id}`
-        }));
+        return NextResponse.json({
+            error: `Invalid order id: ${id}`
+        });
     }
-    //   }
-    // }
-
 }
 
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
-    let bearerToken = (request.headers.get('authorization'))
-    // if (!bearerToken) {
-    //     return new NextResponse('This endpoint requires bearer token');
-    // } else {
-    const token = bearerToken?.split(' ')[1];
-    // let data = await verifyTokenCall(token);
-    // if (data.status == 'invalid') {
-    //     return new NextResponse("Invalid Token");
-    // } else {
+
     let id = parseInt(params.id);
 
     if (!isNaN(id)) {
-        const req = await request.json();
+        let req: any = {}
+        try {
+            req = await request.json();
+        } catch (e) {
+            return NextResponse.json({ "error": "No body found with request" });
+        }
 
         if (req.customerName) {
+            // Authorization already verified by middleware but we still need the token to get data
+            const bearerToken = (request.headers.get('authorization'))
+            const token = bearerToken?.split(' ')[1];
+
             const conn = postgres({
                 ssl: require,
             });
@@ -117,22 +94,25 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 
             if (result.length > 0) {
                 const result = await conn.unsafe(`UPDATE orders SET customerName = '${req.customerName}' WHERE orderid = ${id} AND clientId = ${clientId}`);
-                return new NextResponse("Updated");
+
+                return NextResponse.json({
+                    message: `Order with id no ${id} updated successfully`
+                });
             } else {
-                return new NextResponse(JSON.stringify({
-                    error: `No order with id ${id}`
-                }));
+                return NextResponse.json({
+                    error: `No order exists with id no. ${id}`
+                });
             }
 
         } else {
-            return new NextResponse('Please include customerName in the POST request');
+            return NextResponse.json({
+                error: 'Missing body parameter customerName'
+            });
         }
 
     } else {
-        return new NextResponse(JSON.stringify({
-            error: `Invalid id ${id}`
-        }));
+        return NextResponse.json({
+            error: `Invalid order id: ${id}`
+        });
     }
-    //     }
-    // }
 }
--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,84 +1,68 @@
 import { NextRequest, NextResponse } from "next/server";
 import postgres from "postgres";
 
-// async function verifyTokenCall(token: string) {
-//   const response = await fetch(
-//     `http://localhost:3000/api/verifyToken`,
-//     {
-//       body: JSON.stringify({ "token": token }),
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       method: 'POST'
-//     }
-//   );
-//   return response.json();
-// }
 
 export async function GET(request: NextRequest) {
+  // Authorization already verified by middleware but we still need the token to get data
   let bearerToken = (request.headers.get('authorization'))
-  // if (!bearerToken) {
-  //   return new NextResponse('This endpoint requires bearer token');
-  // } else {
   const token = bearerToken?.split(' ')[1];
-  // let data = await verifyTokenCall(token);
 
-  // if (data.status == 'invalid') {
-  //   return new NextResponse("Invalid Token");
-  // } else {
   const conn = postgres({
     ssl: require,
   });
+
   const result = await conn.unsafe(`SELECT clientId FROM clients where token = '${token}'`);
   let clientId = +result[0]["clientid"];
 
   const orders = await conn.unsafe(`SELECT * FROM orders where clientid = ${clientId}`);
-  return new NextResponse(JSON.stringify(orders));
-  // }
-  // }
+
+  return NextResponse.json({ "orders": orders, "count": orders.length });
 }
 
 export async function POST(request: NextRequest) {
-  const req = await request.json();
-
-  let bearerToken = (request.headers.get('authorization'))
-  // if (!bearerToken) {
-  //   return new NextResponse('This endpoint requires bearer token');
-  // } else 
+  let req: any = {}
+  try {
+    req = await request.json();
+  } catch (e) {
+    return NextResponse.json({ "error": "No body found with request" });
+  }
+  
   if (req.bookId && req.customerName) {
 
-    // verify token
+    // Authorization already verified by middleware but we still need the token to get data
+    let bearerToken = (request.headers.get('authorization'))
     const token = bearerToken?.split(' ')[1];
-    // let data = await verifyTokenCall(token);
 
-    // if (data.status == 'invalid') {
-    //   return new NextResponse("Invalid Token");
-    // } else {
     const conn = postgres({
       ssl: require,
     });
     const bookAvailable = await conn.unsafe(`SELECT * FROM books where id = '${req.bookId}'`);
+
     if (bookAvailable.length == 0) {
-      return new NextResponse(JSON.stringify({
+      return NextResponse.json({
         error: `No book with id ${req.bookId}`
-      }));
+      });
     }
 
     const result = await conn.unsafe(`SELECT clientId FROM clients where token = '${token}'`);
 
     let clientId = +result[0]["clientid"];
 
-    // orderid
-    const totalCounts = await conn.unsafe(`SELECT Count(*) FROM orders where clientId = '${clientId}'`);
-    let count = (+totalCounts[0]["count"]);
-    const entry = await conn.unsafe(`INSERT INTO orders (clientId, bookId, customerName, orderId) VALUES (${clientId}, ${req.bookId}, '${req.customerName}', ${++count});`);
+    // generate new orderID, check, save it, and then send response
+    let orderID = Math.floor(Math.random() * 9000000) + 1000000;
+    let exists = await conn.unsafe(`SELECT * FROM orders where orderId = ${orderID}`);
+    while (exists.length !== 0) {
+      orderID = Math.floor(Math.random() * 9000000) + 1000000;
+      exists = await conn.unsafe(`SELECT * FROM orders where orderId = ${orderID}`);
+    }
 
-    return new NextResponse(JSON.stringify({
-      orderId: count
-    }));
-    // }
+    const entry = await conn.unsafe(`INSERT INTO orders (clientId, bookId, customerName, orderId) VALUES (${clientId}, ${req.bookId}, '${req.customerName}', ${orderID});`);
+
+    return NextResponse.json({
+      orderID
+    });
   }
   else {
-    return new NextResponse('Please include bookId & customerName in the POST request');
+    return NextResponse.json({ "error": "bookId or customerName not found. Please include bookId & customerName in your request" });
   }
 }
